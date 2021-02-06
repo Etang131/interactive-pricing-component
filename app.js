d378ffdaef5168f9c6f4823a570674e64fc6a47b
Erik Tangvik <erikjt@outlook.com>
2021-02-05 22:14:31 -0600

    refactored slider change

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 /*Slider styles */
-var slider = document.getElementById("myRange");
+var sliderPrice = document.getElementById("myRange");
+var output = document.getElementById("myRange").value;
 
-slider.addEventListener("mousemove", function () {
-  var x = slider.value;
-  var color = "linear-gradient(90deg, #a4f3eb" + x + "%, #ecf0fb" + x + "%)";
-  slider.style.background = color;
+//Make switch statement to get var value to show for the price
+/*
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+*/
+
+var start_value = sliderPrice.getAttribute("value");
+
+var x = start_value;
+var color =
+  "linear-gradient(90deg, rgb(164, 243, 235)" +
+  x +
+  "% , rgb(236, 240, 251)" +
+  x +
+  "%)";
+sliderPrice.style.background = color;
+
+sliderPrice.addEventListener("mousemove", function () {
+  x = sliderPrice.value;
+  color =
+    "linear-gradient(90deg, rgb(164, 243, 235)" +
+    x +
+    "% , rgb(236, 240, 251)" +
+    x +
+    "%)";
+  sliderPrice.style.background = color;
 });
